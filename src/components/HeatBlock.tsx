26793fcfd23e8e40cb4c1a21ab486438b14e119a
Amala <amala@amalas-air.lan>
2023-03-03 19:52:21 +1300

    Empty message for HeatBlock component

--- src/components/HeatBlock.tsx
+++ src/components/HeatBlock.tsx
@@ -1,7 +1,26 @@
-import { Divider, Layout, Text } from "@ui-kitten/components";
+import { Divider, Icon, Layout, Text } from "@ui-kitten/components";
 import { CattleList } from "src/types";
 import styled from "styled-components/native";
 import { HeatItem } from "./HeatItem";
+
+const EmptyContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  gap: 10px;
+  justify-content: center;
+  padding: 20px;
+`;
+
+const EmptyMessage: React.FC = () => (
+  <EmptyContainer>
+    <Icon
+      name="checkmark-circle-2-outline"
+      fill="#5ffc99"
+      style={{ width: 20, height: 20 }}
+    />
+    <Text appearance="hint">Nothing to take care of here!</Text>
+  </EmptyContainer>
+);
 
 const Container = styled(Layout)`
   padding-top: 20px;
@@ -26,9 +45,11 @@ export const HeatBlock: React.FC<Props> = (props) => {
     <Container>
       <Text>{label}</Text>
       <ItemsContainer>
-        {cattles.map((cattle) => (
-          <HeatItem cattle={cattle} key={cattle.id} />
-        ))}
+        {cattles.length ? (
+          cattles.map((cattle) => <HeatItem cattle={cattle} key={cattle.id} />)
+        ) : (
+          <EmptyMessage />
+        )}
       </ItemsContainer>
       <Divider />
     </Container>
